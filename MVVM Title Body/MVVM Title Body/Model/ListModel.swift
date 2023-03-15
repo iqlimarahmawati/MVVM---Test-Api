5e2b89efde85e5c3d276ecbd4b6cf5fc86437652
Iqlima Rahmawati <80628223+iqlimarahmawati@users.noreply.github.com>
2023-03-15 08:34:23 +0700

    Add files via upload

---
+++ MVVM Title Body/MVVM Title Body/Model/ListModel.swift
@@ -0,0 +1,14 @@
+//
+//  ListModel.swift
+//  MVVM Title Body
+//
+//  Created by Phincon on 14/03/23.
+//
+
+import Foundation
+struct UserPostModel: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
